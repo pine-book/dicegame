7192a0c08a46077b2b20b197602c6ad02d9c95ef
pine-book <tatsumi719@gmail.com>
2021-07-06 16:49:06 +0900

    Won/lost message added

--- dice_game.py
+++ dice_game.py
@@ -8,4 +8,7 @@ print("Hello, " + name)
 d1 = saikoro()
 d2 = saikoro()
 print("Rolling the dice...\nDie 1: %d\nDie 2: %d\nTotal value: %d" % (d1, d2, d1+d2))
-
+if(d1 + d2 > 7):
+    print("you won!")
+else:
+    print("you lost!")

9aa7605f84b7b9bf770c632f73a606d6b301894b
pine-book <tatsumi719@gmail.com>
2021-07-06 16:38:19 +0900

    Initial revision

---
+++ dice_game.py
@@ -0,0 +1,11 @@
+import random
+def saikoro():
+    return random.randint(1,6)
+
+#print("What is your name ?")
+#name = input()
+#print("Hello, " + name)
+d1 = saikoro()
+d2 = saikoro()
+print("Rolling the dice...\nDie 1: %d\nDie 2: %d\nTotal value: %d" % (d1, d2, d1+d2))
+
